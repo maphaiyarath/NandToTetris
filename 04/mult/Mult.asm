ba1086b61640fab036ec042cca9b40725c71ee19
Michelle Aphaiyarath <maphaiyarath@gmail.com>
2015-10-30 12:45:04 -0500

    Implemented Mult.

--- 04/mult/Mult.asm
+++ 04/mult/Mult.asm
@@ -6,4 +6,26 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Put your code here.
+@R2		  // product's mem. location
+M = 0     // product = 0 (initialization)
+
+(LOOP)
+@R0       // loop count's mem. location
+D = M     // D = loop count
+@STOP     // stop if loop count = 0
+D; JEQ
+
+@R1       // data's mem. location
+D = M     // D = data
+
+@R2
+M = D + M // total = total + data
+
+@R0
+M = M - 1 // decrement loop
+@LOOP     // jump back up to loop if count > 0
+0; JMP
+
+(STOP)
+@STOP
+0; JMP
