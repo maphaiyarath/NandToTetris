3c70fb5f7f0ae4417d8e9aa18c2fea528b6c0029
Michelle Aphaiyarath <maphaiyarath@gmail.com>
2015-12-04 02:01:18 -0600

    Began code for command line arguments.

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,6 +1,9 @@
 #! /usr/bin/env ruby
 
-puts "Hey, what's for lunch?"
-answer = gets.chomp
+asm_filename = ARGV[0]
+puts asm_filename
+
+# puts "Hey, what's for lunch?"
+# answer = gets.chomp
 # p name
-puts "Oh, #{answer}, sounds good!"
+# puts "Oh, #{answer}, sounds good!"
