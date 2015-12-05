c7e2253b8314af4ac0acbbefb25d6f62f152014f
Michelle Aphaiyarath <maphaiyarath@gmail.com>
2015-12-04 23:57:36 -0600

    Refactored driver. Encapsulated .hack filename generation.

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,6 +1,5 @@
 #! /usr/bin/env ruby
 
-# function
 def args_valid?()
   ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1
 end
@@ -9,7 +8,12 @@ def is_readable?(path)
   File.readable?(path)
 end
 
-# unless = opposite of if
+def hack_filename(asm_filename)
+  asm_basename = File.basename(asm_filename, '.asm')
+  path = File.split(asm_filename)[0]
+  "#{path}/#{asm_basename}.hack"
+end
+
 unless args_valid?
   abort ("Usage: ./assembler.rb Prog.asm")
 end
@@ -21,10 +25,7 @@ unless is_readable?(asm_filename)
 end
 
 File.open(asm_filename) do |asm_file|
-  asm_basename = File.basename(asm_filename, '.asm')
-  path = File.split(asm_filename)[0]
-  hack_filename = "#{path}/#{asm_basename}.hack"
-  File.open(hack_filename, 'w') do |hack_file|
+  File.open(hack_filename(asm_filename), 'w') do |hack_file|
     # assembler = Assembler.new(asm_file, hack_file)
     # assembler.assemble
   end
