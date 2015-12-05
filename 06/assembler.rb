b214e5277837861b1de7d9e8cb725015837c8943
Michelle Aphaiyarath <maphaiyarath@gmail.com>
2015-12-04 20:52:54 -0600

    Finished argument validation.

--- 06/assembler.rb
+++ 06/assembler.rb
@@ -1,13 +1,34 @@
 #! /usr/bin/env ruby
 
-if (ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1 && File.exist?(ARGV[0]) && File.readable?(ARGV[0]))
-  asm_filename = ARGV[0]
-  puts asm_filename
-else
+# function
+def args_valid?()
+  ARGV[0] && ARGV[0].end_with?(".asm") && ARGV.length == 1
+end
+
+def is_readable?(path)
+  File.readable?(path)
+end
+
+unless args_valid?
   abort ("Usage: ./assembler.rb Prog.asm")
 end
+
+asm_filename = ARGV[0]
+
+unless is_readable?(asm_filename)
+  abort("#{asm_filename} not found or is unreadable.")
+end
+
+puts "The contents of #{asm_filename}"
 
 # puts "Hey, what's for lunch?"
 # answer = gets.chomp
 # p name
 # puts "Oh, #{answer}, sounds good!"
+
+
+
+
+
+
+
