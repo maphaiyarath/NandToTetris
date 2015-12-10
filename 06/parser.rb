52f104371dc8cc387aee4a0689ef6fba129873ed
Michelle Aphaiyarath <maphaiyarath@gmail.com>
2015-12-09 23:25:02 -0600

    Added parse method and wrote instructions to .hack file.

--- 06/parser.rb
+++ 06/parser.rb
@@ -1,7 +1,11 @@
 class Parser
 	def initialize(assembly_instructions)
 		@assembly_instructions = assembly_instructions
-		puts @assembly_instructions
+		@machine_instructions = []
+	end
+
+	def parse
+		@machine_instructions
 	end
 
 end
